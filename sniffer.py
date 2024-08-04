21193caedc9c663c79f5aa3383c4d0f02ffc7d34
Nipun Anupama <nipun.anupama@gmail.com>
2024-08-04 16:05:13 +0530

    Bug Fixed

--- sniffer.py
+++ sniffer.py
@@ -1,7 +1,6 @@
 #! /usr/bin/python
 
 from ctypes import *
-#import ctypes
 import socket
 import struct
 
@@ -10,15 +9,15 @@ class IP(Structure):
     _fields_ = [
         ("version",          c_ubyte, 4),
         ("ihl",              c_ubyte, 4),
-        ("tos",              c_ubyte),
-        ("len",              c_ushort),
-        ("id",               c_ushort),
-        ("offset",           c_ushort),
-        ("ttl",              c_ubyte),
-        ("protocol_num",     c_ubyte),
-        ("sum",              c_ushort),
-        ("src",              c_uint32),
-        ("dst",              c_uint32)
+        ("tos",              c_ubyte, 8),
+        ("len",              c_ushort,16),
+        ("id",               c_ushort,16),
+        ("offset",           c_ushort,16),
+        ("ttl",              c_ubyte, 8),
+        ("protocol_num",     c_ubyte, 8),
+        ("sum",              c_ushort,16),
+        ("src",              c_uint32,32),
+        ("dst",              c_uint32,32)
         ]
 
     def __new__(self, socket_buffer=None):
@@ -30,12 +29,11 @@ class IP(Structure):
         self.protocol_map = {
                 1:"ICMP",
                 6:"TCP",
-                17:"UDP",
-                142:"Unknown Protocol"
+                17:"UDP"
                 }
         
         # Human Readble form
-        self.src_address = socket.inet_ntoa(struct.pack("!I",self.src))
+        self.src_address = socket.inet_ntoa(struct.pack("@I",self.src))
         self.dst_address = socket.inet_ntoa(struct.pack("!I",self.dst))
 
         try:
@@ -43,8 +41,7 @@ class IP(Structure):
         except:
             self.protocol = str(self.protocol_num)
 
-        print("pp: ", self.protocol_num)
-
+#s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
 s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(0x0800))
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(("eth0", 0))
@@ -53,18 +50,15 @@ try:
     while True:
         # Reading Packet
         resp = s.recvfrom(65565)[0]
-
+        #print(resp)
+        #print(IP)
+        
         # Creating an IP header from the first 20 bytes of the buffer
-        ip = IP(resp[:20])
-        print("RAW: ", ip)
+        ip = IP(resp[14:])
+        #print("RAW: ", ip)
 
         # Print out protocol and the host
-        print("Protocol: %s %s -> %s" % (ip.protocol_num, ip.src_address, ip.dst_address))
-        #print(ip.src)
-        #print(ip.src_address)
-        #print(ip.dst)
-        #print(ip.dst_address)
-
+        print("Protocol: %s %s -> %s" % (ip.protocol, ip.src_address, ip.dst_address))
 except KeyboardInterrupt:
     pass
 
